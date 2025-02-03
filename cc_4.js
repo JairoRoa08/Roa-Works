af9a17130dbc7ba226b4cf5e86b72613129b9683
Jairo Roa <Jroa@usf.edu>
2025-02-02 23:21:31 -0500

    Task 1 - Customer Discounts

---
+++ cc_4.js
@@ -0,0 +1,10 @@
+// Task 1
+let purchaseAmount = 120;
+let discount = 0;
+if (purchaseAmount > 100) {
+    discount = purchaseAmount * 0.2;
+}
+
+let finalAmount = purchaseAmount - discount;
+console.log(`Final amount after discount: $${finalAmount.toFixed(2)}`);
+
