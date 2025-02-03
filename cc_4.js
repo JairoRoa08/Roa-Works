3d999e7e1ab2313a527167bcd2e250dfce4062d4
Jairo Roa <Jroa@usf.edu>
2025-02-02 23:28:36 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -9,4 +9,12 @@ let finalAmount = purchaseAmount - discount;
 console.log(`Final amount after discount: $${finalAmount.toFixed(2)}`);
 
 // Task 2
+let sales= [10, 20, 30, 40, 50];
+let totalSales = 0;
+for (let i =0; i< sales.length; i++) {
+    totalSales += sales[i];
+}
+
+console.log('Total Sales: $$(totalSales)');
+
 
